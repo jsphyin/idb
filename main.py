086c23762720169489fac40cbe60548c4e37f215
Anurag Bakshi <anuragbakshi123@gmail.com>
2017-09-25 23:21:37 -0500

    Add basic about page

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     # Run locally in debug mode (gunicorn runs the app in production)
