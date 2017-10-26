ffc8c28c4d0a827d70a044eea11cd02fd440bda5
Alexander Lo <alexander.lo@utexas.edu>
2017-10-25 22:53:39 -0500

    Started unit tests

---
+++ tests.py
@@ -0,0 +1,9 @@
+from unittest import main, TestCase
+from models import db, Game, Family, Genre, Publisher, Artist, Developer, Mechanic
+
+class TestAPI(TestCase):
+    def test(self):
+        pass
+
+if __name__ == "__main__":
+    main()
