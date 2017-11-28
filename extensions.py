b397e3f0af0c78842e4dc275f0f8b6b3fe825693
Anurag Bakshi <anuragbakshi123@gmail.com>
2017-11-28 13:37:09 -0600

    Add CORS headers

--- extensions.py
+++ extensions.py
@@ -1,9 +1,11 @@
 import os
 
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
+CORS(app)
 
 # for access cloud sql from local: export SQLALCHEMY_DATABASE_URI=mysql+pymysql://root:boardgamers@127.0.0.1:3306/proddata
 # for creating db locally: export SQLALCHEMY_DATABASE_URI=sqlite:////tmp/boardgamedb.sqlite
